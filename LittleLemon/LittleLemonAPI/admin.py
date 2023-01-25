56efed5f617d792162d69b4032a348f1c7d0dad6
Marina Saitgareeva <4006761ms@gmail.com>
2023-01-25 09:29:00 -0500

    delete cart, orderitem from admin

--- LittleLemon/LittleLemonAPI/admin.py
+++ LittleLemon/LittleLemonAPI/admin.py
@@ -5,6 +5,4 @@ from .models import Category, MenuItem, Cart, Order, OrderItem
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(MenuItem)
-admin.site.register(Cart)
 admin.site.register(Order)
-admin.site.register(OrderItem)
