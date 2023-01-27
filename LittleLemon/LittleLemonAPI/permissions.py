b904964be77fe247b63af6189696c8b01a96cefd
Marina Saitgareeva <4006761ms@gmail.com>
2023-01-27 08:50:08 -0500

    add customer and delivery_crew object owner permission

--- LittleLemon/LittleLemonAPI/permissions.py
+++ LittleLemon/LittleLemonAPI/permissions.py
@@ -18,13 +18,20 @@ class IsManager(BasePermission):
 class IsDeliveryCrew(BasePermission):
     def has_permission(self, request, view):
         return request.user.groups.filter(name="Delivery Crew").exists()
+    
+    
+class IsDeliveryCrewAndOwner(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="Delivery Crew").exists()
 
+    def has_object_permission(self, request, view, obj):
+        return obj.delivery_crew == request.user
 
 class ReadOnly(BasePermission):
     def has_permission(self, request, view):
         # return (request.method in SAFE_METHODS) and request.user.is_authenticated
         return request.method in SAFE_METHODS
-
+    
 
 class IsCustomer(BasePermission):
     def has_permission(self, request, view):
@@ -32,3 +39,14 @@ class IsCustomer(BasePermission):
         return request.user.is_authenticated and (not IsAdminUser)
         # user, not belonging to any group = Customer
         # return request.user.is_authenticated and self.request.user.groups.count() == 0
+
+
+class IsCustomerAndOwner(BasePermission):
+    def has_permission(self, request, view):
+        # The IsAdminUser permission class will deny permission to any user, unless user.is_staff is True in which case permission will be allowed
+        return request.user.is_authenticated and (not IsAdminUser)
+        # user, not belonging to any group = Customer
+        # return request.user.is_authenticated and self.request.user.groups.count() == 0
+        
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
