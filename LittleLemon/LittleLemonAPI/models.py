04161e7c883b206e0dcf96a37eb46327402b904e
Marina Saitgareeva <4006761ms@gmail.com>
2023-01-27 09:51:46 -0500

    add fields in models

--- LittleLemon/LittleLemonAPI/models.py
+++ LittleLemon/LittleLemonAPI/models.py
@@ -5,17 +5,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     slug = models.SlugField()
-    title = models.CharField(max_length=255, db_index=True)
+    title = models.CharField(max_length=255, db_index=True, blank=None, null=None)
     
     def __str__(self):
         return self.title
     
 
 class MenuItem(models.Model):
-    title = models.CharField(max_length=255, db_index=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
-    featured = models.BooleanField(db_index=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    title = models.CharField(max_length=255, db_index=True, blank=None, null=None)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True, blank=None, null=None)
+    featured = models.BooleanField(db_index=True, blank=None, null=None)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, blank=None, null=None)
     
     def __str__(self):
         return str(self.title) + " (" + str(self.category) + ")"
@@ -23,8 +23,8 @@ class MenuItem(models.Model):
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    quantity = models.SmallIntegerField()
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE, blank=None, null=None)
+    quantity = models.SmallIntegerField(blank=None, null=None)
     # price = models.DecimalField(max_digits=6, decimal_places=2)
     # subtotal = models.DecimalField(max_digits=6, decimal_places=2)
     
@@ -44,9 +44,10 @@ class Order(models.Model):
         null=True,
         limit_choices_to={"groups__name": "Delivery Crew"}
     )
-    status = models.BooleanField(db_index=True, default=0)
-    total = models.DecimalField(max_digits=6, decimal_places=2)
-    date = models.DateField(db_index=True)
+    status = models.BooleanField(db_index=True, default=0, blank=None, null=None)
+    total = models.DecimalField(max_digits=6, decimal_places=2, blank=None, null=None)
+    # auto_now_add=True -> Automatically set the field to now when the object is first created
+    date = models.DateField(db_index=True, auto_now_add=True, blank=None, null=None)
     
     def __str__(self):
         return str(self.user) + " (Order# " + str(self.id) + ")"
@@ -54,8 +55,8 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    quantity = models.SmallIntegerField()
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE, blank=None, null=None)
+    quantity = models.SmallIntegerField(blank=None, null=None)
     # unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     # price = models.DecimalField(max_digits=6, decimal_places=2)
     
